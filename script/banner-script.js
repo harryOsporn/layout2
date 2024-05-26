3061789eae4c3d7ddfc7cff75318c51f77622e15
harryOsborn <honghi.hunh@gmail.com>
2024-05-26 08:36:27 +0700

    FormDangNhap,FormDangKy

--- script/banner-script.js
+++ script/banner-script.js
@@ -27,6 +27,6 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 1500); 
+  setTimeout(showSlides, 2000); 
 }
 
